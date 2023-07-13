9896c67fdf77d2d4f529154cbf9a1bf5bd370a31
sebastianvallej0 <123136301+sebastianvallej0@users.noreply.github.com>
2023-07-12 21:56:03 -0500

    Update ejpunteros.c

--- ejpunteros.c
+++ ejpunteros.c
@@ -1,113 +1,118 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 int main()
 {
-    char nombre [50];
-    char cedula [20];
-    float cantidad1 =0 ,cantidad2 = 0,cantidad3 = 0 ,cantidad4=0 ,cantidad5 = 0;
-    float precio1 = 100, precio2 =60 ,precio3 = 200,precio4 = 50 ,precio5 = 90;
-    float subtotal1 = 0 ,subtotal2 = 0 , subtotal3 = 0 ,subtotal4 = 0, subtotal5 = 0;
-    float subtotal = 0, total = 0;
-   
+    char nombre_cliente[50];
+    char direccion_cliente[100];
+    char numero_cliente[10];
+    float cantidades[5] = {0};
+    float precios[5] = {1.5, 0.95, 3.5, 2, 4};
+    float subtotales[5] = {0};
+    float subtotal_total = 0, total = 0;
+
     char opcion;
 
-    printf("ELIJA UN PRODUCTO \n");
-    printf("a)Tarjeta Madre (Precio: $150) \n");
-    printf("b)Memorias Ram (Precio: $55)\n");
-    printf("c)procesador (Precio: $180) \n");
-    printf("d)Fuente de poder (Precio: $70)\n");
-    printf("e)Refrigeracion liquida (Precio: $120) \n");
+    printf("Seleccione una opcion de compra:\n");
+    printf("a) Atun (Precio: $1.5)\n");
+    printf("b) Arroz (Precio: $0.95)\n");
+    printf("c) Aceite (Precio: $3.5)\n");
+    printf("d) Azucar (Precio: $2)\n");
+    printf("e) harina (Precio: $4)\n");
 
-    printf("Ingrese la letra del producto a facturar \n");
-    scanf("%c",&opcion);
+    printf("Ingrese la letra del producto a facturar: ");
+    scanf(" %c", &opcion);
 
     switch (opcion)
     {
     case 'a':
-        printf("Ingrese la cantidad de Tarjeta Madre que desea \n");
-        scanf("%f",&cantidad1);
-
+        printf("Ingrese la cantidad de Atunes que desea: ");
+        scanf("%f", &cantidades[0]);
         break;
     case 'b':
-        printf("Ingrese la cantidad de Memorias RAM que desea \n");
-        scanf("%f",&cantidad2);
+        printf("Ingrese la cantidad de Libras de arroz que desea: ");
+        scanf("%f", &cantidades[1]);
         break;
     case 'c':
-        printf("Ingrese la cantidad de Procesadores que desea \n");
-        scanf("%f",&cantidad3);
-   
+        printf("Ingrese la cantidad de litros de aceite que desea: ");
+        scanf("%f", &cantidades[2]);
         break;
     case 'd':
-        printf("Ingrese la cantidad de Fuentes de Poder que desea \n");
-        scanf("%f",&cantidad4);
-   
+        printf("Ingrese la cantidad de libras de azucar que desea: ");
+        scanf("%f", &cantidades[3]);
         break;
     case 'e':
-        printf("Ingrese la cantidad de Refrigeraciones Liquidas que desea \n");
-        scanf("%f",&cantidad5);
-   
+        printf("Ingrese la cantidad de libras de harina que desea: ");
+        scanf("%f", &cantidades[4]);
         break;
     default:
         break;
     }
 
-    subtotal1 = (precio1*cantidad1);
-    subtotal2 = (precio2*cantidad2);
-    subtotal3 = (precio3*cantidad3);
-    subtotal4 = (precio4*cantidad4);
-    subtotal5 = (precio5*cantidad5);
-    subtotal = subtotal1 + subtotal2+ subtotal3 + subtotal4 + subtotal5;
+    for (int i = 0; i < 5; i++)
+    {
+        subtotales[i] = precios[i] * cantidades[i];
+        subtotal_total += subtotales[i];
+    }
 
-    if (subtotal > 100 && subtotal <= 500)
+    if (subtotal_total > 10 && subtotal_total <= 50)
     {
-        total = subtotal * 0.95;
+        total = subtotal_total * 0.95;
     }
-    else if (subtotal >500 && subtotal <=1000)
+    else if (subtotal_total > 50 && subtotal_total <= 100)
     {
-        total = subtotal * 0.93;
+        total = subtotal_total * 0.93;
     }
-    else if (subtotal>1000)
+    else if (subtotal_total > 100)
     {
-        total = subtotal * 0.90;
+        total = subtotal_total * 0.90;
     }
     else
     {
-        total = subtotal;
+        total = subtotal_total;
     }
-    printf("Ingrese su nombre\n");
-    scanf("%s",&nombre);
-    printf("Ingrese su cedula\n");
-    scanf("%s",&cedula);
 
-    printf("* DATOS DEL CLIENTE***\n");
-    printf("Nombre: %s \n",nombre);
-    printf("Cedula: %s \n",cedula);
+    printf("Ingrese su nombre: ");
+    scanf("%s", nombre_cliente);
+    printf("Ingrese su numero de telefono: ");
+    scanf("%s", numero_cliente);
+    printf("Ingrese su direccion: ");
+    scanf("%s", direccion_cliente);
 
-    printf("*FACTURA \n");
-    printf("PRODUCTO                        CANTIDAD           VALOR UNITARIO          TOTAL \n");
-    if (cantidad1 != 0)
+    printf("\n* DATOS DEL CLIENTE *\n");
+    printf("Nombre: %s\n", nombre_cliente);
+    printf("Numero: %s\n", numero_cliente);
+    printf("Direccion: %s\n", direccion_cliente);
+
+    printf("\n* FACTURA *\n");
+    printf("PRODUCTO\t\tCANTIDAD\tVALOR UNITARIO\t\tTOTAL\n");
+
+    if (cantidades[0] != 0)
     {
-        printf("Tarjeta Madre                          %.0f                   %2.f                   %2.f\n", cantidad1,precio1,subtotal1);
+        printf("Atun\t\t%.0f\t\t$%.2f\t\t$%.2f\n", cantidades[0], precios[0], subtotales[0]);
     }
-    if (cantidad2 != 0)
+    if (cantidades[1] != 0)
     {
-        printf("Memorias RAM             %.0f                   %2.f                    %2.f\n", cantidad2,precio2,subtotal2);
+        printf("Arroz\t\t%.0f\t\t$%.2f\t\t$%.2f\n", cantidades[1], precios[1], subtotales[1]);
     }
-    if (cantidad3 != 0)
+    if (cantidades[2] != 0)
     {
-        printf("Procesadores                %.0f                   %2.f                     %2.f\n", cantidad3,precio3,subtotal3);
+        printf("Aceite\t\t%.0f\t\t$%.2f\t\t$%.2f\n", cantidades[2], precios[2], subtotales[2]);
     }
-    if (cantidad4 != 0)
+    if (cantidades[3] != 0)
     {
-        printf("Fuentes de poder                           %.0f                   %2.f                     %2.f\n", cantidad4,precio4,subtotal4);
+        printf("azucar\t%.0f\t\t$%.2f\t\t$%.2f\n", cantidades[3], precios[3], subtotales[3]);
     }
-    if (cantidad5 != 0)
+    if (cantidades[4] != 0)
     {
-        printf("Refrigeraciones Liquidas                      %.0f                    %2.f                   %2.f\n", cantidad5,precio5,subtotal5);
+        printf("harina\t%.0f\t\t$%.2f\t\t$%.2f\n", cantidades[4], precios[4], subtotales[4]);
     }
-    printf("El subtotal es:%2.f \n", subtotal);
-    printf("El total es:%2.f \n", total);
+
+    printf("\nEl subtotal es: $%.2f\n", subtotal_total);
+    printf("El total es: $%.2f\n", total);
+
     system("pause");
+    return 0;
+}
 
-    }
     
